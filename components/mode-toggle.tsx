5fb96376582a3689a1052e1f201883a24bd548e5
Mickael Sermont <sermont.mickael@hotmail.fr>
2023-07-19 10:13:10 +0200

    feat: add animated mode toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,44 +1,89 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
+import { useTheme } from "next-themes";
+import { AnimatePresence, motion } from "framer-motion";
+
+const SunIcon = () => (
+  <>
+    <motion.svg
+      width="22"
+      height="22"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      initial={{ scale: 0.5, opacity: 0, rotate: 90 }}
+      animate={{
+        scale: 1,
+        opacity: 1,
+        rotate: 0,
+        transition: { duration: 0.2, type: "spring", stiffness: 100 },
+      }}
+      exit={{
+        scale: 0.5,
+        opacity: 0,
+        rotate: 90,
+        transition: { duration: 0.2 },
+      }}
+    >
+      <circle cx="12" cy="12" r="5"></circle>
+      <line x1="12" y1="1" x2="12" y2="3"></line>
+      <line x1="12" y1="21" x2="12" y2="23"></line>
+      <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+      <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+      <line x1="1" y1="12" x2="3" y2="12"></line>
+      <line x1="21" y1="12" x2="23" y2="12"></line>
+      <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+      <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+    </motion.svg>
+  </>
+);
+
+const MoonIcon = () => (
+  <>
+    <motion.svg
+      width="22"
+      height="22"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      initial={{ scale: 0.5, opacity: 0, rotate: 90 }}
+      animate={{
+        scale: 1,
+        opacity: 1,
+        rotate: 0,
+        transition: { duration: 0.2, type: "spring", stiffness: 100 },
+      }}
+      exit={{
+        scale: 0.5,
+        opacity: 0,
+        rotate: 90,
+        transition: { duration: 0.2 },
+      }}
+    >
+      <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+    </motion.svg>
+  </>
+);
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme, theme } = useTheme();
 
   return (
     <button
+      role="button"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="border rounded-md w-6 h-6 flex items-center justify-center">
+      className="p-2 min-h-[40px] -mr-2 block focus:outline-none"
+    >
       <span className="sr-only">Toggle mode</span>
-      {theme !== "dark" ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-4 h-4">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-          />
-        </svg>
-      ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-4 h-4">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-          />
-        </svg>
-      )}
+      <AnimatePresence initial={false}>
+        {theme !== "dark" ? <SunIcon /> : <MoonIcon />}
+      </AnimatePresence>
     </button>
-  )
+  );
 }
