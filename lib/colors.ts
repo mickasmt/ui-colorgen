2cff87a1240bb936b49d030411ed1e5e40ec66ff
Mickael Sermont <sermont.mickael@hotmail.fr>
2023-07-28 16:38:21 +0200

    feat: update preview code tailwind.config

--- lib/colors.ts
+++ lib/colors.ts
@@ -251,27 +251,13 @@ export function generateTailwindCode(
     })
     .join(`\n      `);
 
-  const twCodeJS = `/** @type {import('tailwindcss').Config} */
-module.exports = {
+  const twCodeJS = `module.exports = {
   ...,
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
       ${colorCode}
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      ...,
     },
   },
   plugins: [require("tailwindcss-animate")],
